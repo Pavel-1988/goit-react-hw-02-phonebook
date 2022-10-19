d2c3fd140a781a17addf90cbf51aa14c48c0a805
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2022-10-19 13:26:51 +0300

    second task

---
+++ src/components/App.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { nanoid } from 'nanoid';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+
+
+export class App extends React.Component {
+
+  state = {
+    contacts: [ ],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+    const newContact = { id: nanoid(), name, number };
+    contacts.some(contact => contact.name === name)
+      ? Report.warning(
+          `${name}`,
+          ' is already in the contact.',
+        )
+      :
+      this.setState(({ contacts }) => ({
+          contacts: [newContact, ...contacts],
+        }
+      ));
+    };
+
+  
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalised = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalised)
+    );
+  };
+
+  deleteButton = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+   changeFilter = e => {
+    this.setState({
+      filter: e.currentTarget.value,
+    });
+  };
+
+  render() {
+    const { filter } = this.state;
+    
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm  onSubmit={this.addContact}  />
+
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={this.getVisibleContacts()}
+          onDeleteContact={this.deleteButton}
+        />
+      </>
+    )
+  }
+}
